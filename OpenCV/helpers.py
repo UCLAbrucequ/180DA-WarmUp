01b597ea527e069294dac7d5d5721041c002a68d
UCLAbrucequ <brucequ@ucla.edu>
2023-01-12 20:58:44 -0800

    Changed Py version to 3.8

--- OpenCV/helpers.py
+++ OpenCV/helpers.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-def display(path, flag):
+def display(flag, path=None):
     """
     Displays the image following the path
     INPUT:
@@ -17,3 +17,18 @@ def display(path, flag):
         if k == 27:
             break
     cv.destroyAllWindows()
+
+
+def adaptive_threshold(imgray):
+    result = cv.adaptiveThreshold(imgray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                    cv.THRESH_BINARY, 11, 2)
+    return result
+
+def Otsu_Gaussian(imgray):
+    """
+    Otsu's thresholding after Gaussian filtering
+
+    """
+    blur = cv.GaussianBlur(imgray, (5,5), 0)
+    ret, result = cv.threshold(blur, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    return result
